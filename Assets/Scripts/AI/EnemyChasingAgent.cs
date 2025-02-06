6c5f988cc425e8606e206382399454cab2827470
Duong Hoang Thanh <thanh1807@yahoo.com>
2025-02-06 21:17:57 +0700

    feat: Add Enemy Chasing states

---
+++ Assets/Scripts/AI/EnemyChasingAgent.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChasingAgent : MonoBehaviour
+{
+	[SerializeField] private float _chasingRange;
+	[SerializeField] private float _attackingRange;
+
+	private Enemy _currentEnemy;
+
+	public bool IsInChasingRange => Distance <= _chasingRange;
+	public bool IsInAttackingRange => Distance <= _attackingRange;
+	private float Distance => (_currentEnemy == null) ? float.MaxValue: Vector3.Distance(transform.position, _currentEnemy.transform.position);
+
+	public bool HasEnemy => _currentEnemy != null;
+
+	private void Update()
+	{
+		if (_currentEnemy == null)
+		{
+			LookForNearbyEnemy();
+		}
+		else
+		{
+			CheckIfEnemyStillValid();
+		}
+	}
+
+	private void LookForNearbyEnemy()
+	{
+		var enemies = FindObjectsOfType<Enemy>();
+		foreach (var enemy in enemies)
+		{
+			if (Vector3.Distance(transform.position, enemy.transform.position) < _chasingRange)
+			{
+				_currentEnemy = enemy;
+				return;
+			}
+		}
+	}
+
+	private void CheckIfEnemyStillValid()
+	{
+		if (!IsInChasingRange)
+		{
+			_currentEnemy = null;
+		}
+	}
+}
